1245709fac4a781941ea184d730684f7c18bcbb5
Rachel Washington <rachel.washington@fdmgroup.com>
2023-10-08 18:09:03 -0400

    Launching on Heroku

--- backend/base.py
+++ backend/base.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask_cors import CORS, cross_origin
 
-api = Flask(__name__)
+api = Flask(__name__, static_folder='../frontend/build', static_url_path='')
 
-
+CORS(api)
 @api.route('/home')
+@cross_origin()
 def home_page():
     response_body = {
         "about": "StreamingBuddy"
     }
-
     return response_body
+
+@api.route('/')
+@cross_origin()
+def serve():
+    return send_from_directory(api.static_folder, 'index.html')
